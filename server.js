5ed1b23eaa42f99b94c02eb19177b1d12ae653c1
Wladastic <wlad.c@me.com>
2015-04-19 20:26:58 +0200

    finished the server, running on RAM only, not saving yet

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-/**
- * Created by wladislavcugunov on 19/04/15.
- */
+
 console.log('Get the server starteeeed!');
 var express = require('express');
 var app = express();
-var chat;
+var chat =[];
+/**
+ * @module (config)
+ * @type {exports}
+ */
+var config = require('./private/config.js');
+
+
+console.log(config.server.port);
 
 app.use(express.static('public'));
 
-
-app.get('/', function (req, res)
-    {
-        res.send('Welcome to the Chat, click here to enter: <a href="/public/index.html"> START CHAT</a>');
+app.get('/', function(req,res){
+    res.send(chat);
     });
+
+app.get('/joined', function(req, res){
+    req.query.message;
+    console.log("Anonymous joined.");
+    chat.push('Anynous joined Chat.');
+    res.send(chat);
+});
 
 app.get('/message', function(req, res){
     req.query.message;
-    console.log("Message received by Client: ", req.query.message);
+    console.log("Message received from Client: ", req.query.message);
+    chat.push(req.query.message);
+    res.send(chat);
 });
 
-app.listen(3000);
+
+app.listen(config.server.port);
