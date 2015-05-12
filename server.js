c1a2d110eadd90364c1f6287473be506dc979395
Wladastic <wlad.c@me.com>
2015-05-12 14:51:53 +0200

    Added some color to the Chatterbox, The Date has finally been done right, so that the time can be displayed. to do so, I made a further attribute to the Chathistory model, called clienttime. Clienttime is just a string which consists of the current hour and minute when the user submits a message. Added Joined message, but the server and the client expect a username still, so I added a blank space as username.
    TODO:
    Client Authentification
    Server has to clear the Online User-list somehow

--- server.js
+++ server.js
@@ -5,13 +5,23 @@ var app = express();
 var chat =[];
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
-var ChatHistoryModel = mongoose.model('ChatHistory', {username: String , message: String , date: Date});
+var ChatHistoryModel = mongoose.model('ChatHistory', {username: String , message: String , date: String, clienttime: String });
 var UserListModel = mongoose.model('UserList', {username: String, password: String });
 /**
  * @module (config)
  * @type {exports}
  */
 var config = require('./private/config.js');
+
+var OnlineUsers = [];
+
+var checkUser = function(UsernametoTest) {
+            //Not implemented yet,  will need some work until doing so...
+            for (i=0 ; i < OnlineUsers.length; i++) {
+            if ( OnlineUsers[i] == UsernametoTest ){ return true;}
+        }
+        OnlineUsers[OnlineUsers.length] = UsernametoTest;
+};
 
 console.log('Server running at "localhost:'+ config.server.port+'"');
 
@@ -22,15 +32,14 @@ app.get('/', function(req,res){
     });
 
 app.get('/joined', function(req, res){
-    console.log(req.query.usernamereq + ' requested.');
+    console.log('Join requested by unidentified Client');
     if(req.query.usernamereq.length) {
-
-        var ChatEntry= new ChatHistoryModel({username: 'Server' ,message: req.query.usernamereq + " joined.", date: new Date().getHours()  });
+        var ChatEntry= new ChatHistoryModel({username: ' ' , message:  req.query.usernamereq  + " has joined the Chat.", clienttime: new Date().getHours()+ ":" + new Date().getMinutes()  });
         ChatEntry.save(function (err, saved) {
             if(err) return console.error(err);
-            console.log( req.query.usernamereq + " joined.");
+            console.log( "Client identifies himself as " + req.query.usernamereq + " and has joined.");
             getmessages(res);
-        })
+        });
 
     }
     else {
@@ -39,15 +48,23 @@ app.get('/joined', function(req, res){
 
 });
 
+app.get('/usersonline', function(req, res){
+    if(req.query.usernamereq.length){
+        checkUser(req.query.usernamereq);
+        res.send(OnlineUsers);
+    }
+});
+
 app.get('/refresh', function(req, res){
     if(req.query.usernamereq.length){
+
     getmessages(res);
     }
     else{res.send('Server: Please enter a Username and hit "Join"');}
 });
 
 app.get('/register', function (req,res){
-        req.query.usernamereq;
+        //req.query.usernamereq;
         UserListModel.find({}, 'username', function (err, pass) {
             res.send("Username is available.");
             console.log(req.query.registry);
@@ -57,13 +74,18 @@ app.get('/register', function (req,res){
 );
 
 
+//  Listen for Usernames and add to array that checks every 10 seconds, if each user is still online.
+//  Add function that calls the variable and adds {is_online : yes} to each user, then enters it
+//  into it's username array in the mongodb
+
+
 
 app.get('/message', function(req, res){
-    req.query.message;
+    //req.query.message;
     console.log("Username: " , req.query.username);
     if (req.query.username === false ) { console.log("Please Enter a Username!")}
     else {
-        var ChatEntry= new ChatHistoryModel({message: req.query.message, username:req.query.username  , date: new Date() });
+        var ChatEntry= new ChatHistoryModel({message: req.query.message, username:req.query.username  , clienttime: new Date().getHours() + ":" + new Date().getMinutes() });
             ChatEntry.save(function (err, saved) {
                 if(err) return console.error(err);
                 console.log("Message received from Client->",req.query.username, ":" , req.query.message );
@@ -74,7 +96,7 @@ app.get('/message', function(req, res){
 });
 
 var getmessages = function(res){
-    ChatHistoryModel.find({}, 'message' & 'date' &'username' , function (err, messages) {
+    ChatHistoryModel.find({}, 'message' & 'clienttime'  &'username' , function (err, messages) {
         res.send(messages);
     });
 };
