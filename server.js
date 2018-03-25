5a7506c4f905ac18708d145af414d5859b3b2d2c
Wladastic <wlad.c@me.com>
2018-03-25 22:18:25 +0200

    changed server.js for multiple users partially...

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 console.log('Server started!');
 var express = require('express');
 var app = express();
-var chat =[];
+var chat = [];
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
-var ChatHistoryModel = mongoose.model('ChatHistory', {username: String , message: String , date: String, clienttime: String });
-var UserListModel = mongoose.model('UserList', {username: String, password: String });
+
+// defining models
+var ChatHistoryGlobalModel;
+ChatHistoryGlobalModel = mongoose.model('ChatHistory', {
+    username: String,
+    message: String,
+    date: String,
+    clienttime: String
+});
+var UserListModel;
+UserListModel = mongoose.model('UserList', {
+    username: String,
+    password: String,
+    userId: String,
+    email: String,
+    firstname: String
+});
+
+
 /**
  * @module (config)
  * @type {exports}
@@ -14,29 +31,31 @@ var config = require('./private/config.js');
 
 var OnlineUsers = [];
 
-var checkUser = function(UsernametoTest) {
-            //Not implemented yet,  will need some work until doing so...
-            for (i=0 ; i < OnlineUsers.length; i++) {
-            if ( OnlineUsers[i] == UsernametoTest ){ return true;}
+var checkUser = function (UsernametoTest) {
+    //Not implemented yet,  will need some work until doing so...
+    OnlineUsers[OnlineUsers.length] = UsernametoTest;
+    for (i = 0; i < OnlineUsers.length; i++) {
+        if (OnlineUsers[i] == UsernametoTest) {
+            return true;
         }
-        OnlineUsers[OnlineUsers.length] = UsernametoTest;
-            /* if(new Date().getMinutes % 2 == 0){
-             OnlineUsers = [];
-             };
-             *  */
+    }
+    /* if(new Date().getMinutes % 2 == 0){
+     OnlineUsers = [];
+     };
+     *  */
 };
 
-console.log('Server running at "localhost:'+ config.server.port+'"');
+console.log('Server running at "localhost:' + config.server.port + '"');
 
 app.use(express.static('public'));
 
-app.get('/', function(req,res){
-
-    });
+app.get('/', function (req, res) {
+    console.log(req.getmessages())
+});
 
 
 //TODO: add login and token verification
-//TODO: 
+//TODO:
 app.get('/joined', function(req, res){
     console.log('Join requested by unidentified Client');
     if(req.query.usernamereq.length) {
@@ -53,22 +72,24 @@ app.get('/joined', function(req, res){
     }
 });
 
-app.get('/usersonline', function(req, res){
-    if(req.query.usernamereq.length){
+app.get('/usersonline', function (req, res) {
+    if (req.query.usernamereq.length) {
         checkUser(req.query.usernamereq);
         res.send(OnlineUsers);
     }
 });
 
-app.get('/refresh', function(req, res){
-    if(req.query.usernamereq.length){
+app.get('/refresh', function (req, res) {
+    if (req.query.usernamereq.length) {
 
-    getmessages(res);
+        getmessages(res);
     }
-    else{res.send('Server: Please enter a Username and hit "Join"');}
+    else {
+        res.send('Server: Please enter a Username and hit "Join"');
+    }
 });
 
-app.get('/register', function (req,res){
+app.get('/register', function (req, res) {
         //req.query.usernamereq;
         UserListModel.find({}, 'username', function (err, pass) {
             res.send("Username is available.");
@@ -82,20 +103,22 @@ app.get('/register', function (req,res){
 // TODO: add parameter to who the message is supposed to be sent, then later work with tokens
 app.get('/message', function(req, res){
     //req.query.message;
-    console.log("Username: " , req.query.username);
-    if (req.query.username === false ) { console.log("Please Enter a Username!")}
+    console.log("Username: ", req.query.username);
+    if (req.query.username === false) {
+        console.log("Please Enter a Username!")
+    }
     else {
-        var ChatEntry = new ChatHistoryModel({message: req.query.message, username:req.query.username  , clienttime: new Date().getHours() + ":" + new Date().getMinutes() });
-            ChatEntry.save(function (err, saved) {
-                if(err) return console.error(err);
-                console.log("Message received from Client->",req.query.username, ":" , req.query.message );
-                                   /*  *** PLEASE FIND OUT WHY NOT SAVING  ***
-                                    if ( (req.query.message).indexOf('Jarvis') >= 0) {
-                                        var ChatEntryJarv= new ChatHistoryModel({username: 'Jarvis' , message:  "Yes?" , clienttime: new Date().getHours()+ ":" + new Date().getMinutes()  });
-                                        ChatEntryJarv.save(function(err, saved){if(err) return console.error(err); console.log("Jarvis has responded to user.")});
-                                    } */
-                getmessages(res);
-            })
+        var ChatEntry = new ChatHistoryGlobalModel({message: req.query.message, username: req.query.username, clienttime: new Date()});
+        ChatEntry.save(function (err, saved) {
+            if (err) return console.error(err);
+            console.log("Message received from Client->", req.query.username, ":", req.query.message);
+            /*  *** PLEASE FIND OUT WHY NOT SAVING  ***
+             if ( (req.query.message).indexOf('Jarvis') >= 0) {
+             var ChatEntryJarv= new ChatHistoryGlobalModel({username: 'Jarvis' , message:  "Yes?" , clienttime: new Date().getHours()+ ":" + new Date().getMinutes()  });
+             ChatEntryJarv.save(function(err, saved){if(err) return console.error(err); console.log("Jarvis has responded to user.")});
+             } */
+            getmessages(res);
+        })
     }
 
 });
@@ -108,6 +131,8 @@ var getmessages = function(res){
     });
 };
 
-setInterval(function(){ OnlineUsers = []; }, 5000);
+setInterval(function () {
+    OnlineUsers = [];
+}, 5000);
 
 app.listen(config.server.port);
